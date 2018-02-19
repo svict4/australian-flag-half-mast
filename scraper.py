fe59738c813c7ec45773a34fe18ee428b4d1cfab
Simon <email@simonvictory.com>
2018-02-20 02:20:16 +1100

    added most of scraper code required, TODO seed database

--- scraper.py
+++ scraper.py
@@ -1,6 +1,47 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
+from bs4 import BeautifulSoup
+import requests
+
+PMCTLD = "http://pmc.gov.au"
+FLAGNETWORKLINK = "http://pmc.gov.au/government/australian-national-flag/flag-network"
+
+rFlagNetwork = requests.get(FLAGNETWORKLINK)
+soup = BeautifulSoup(rFlagNetwork.content, "html.parser")
+
+pages = int(soup.select("#block-system-main > div > div > div > div.item-list > ul > li.pager-last.last > a")[0].attrs['href'].split("=")[1])
+
+all_announcements = []
+for page in range(pages):
+    if page != 0:
+        rFlagNetwork = requests.get(FLAGNETWORKLINK + "?page=" + str(page))
+        soup = BeautifulSoup(rFlagNetwork.content, "html.parser")
+
+    page_content1 = soup.select("#block-system-main > div > div > div > div.view-content > div.views-row > div.views-field.views-field-title > h2 > a")
+    page_content2 = soup.select("#block-system-main > div > div > div > div.view-content > div.views-row > div.views-field.views-field-field-action-date > h3 > span")
+
+    page_content = page_content1 + page_content2
+
+    for i in range(-(-len(page_content) // 2)):
+        content = page_content[i::-(-len(page_content) // 2)]
+        all_announcements.append({'title': content[0].get_text(strip=True), 'link': content[0].attrs['href'], 'date': content[1].get_text(strip=True)})
+
+for announcement in all_announcements:
+    rFlagNetwork = requests.get(PMCTLD + announcement['link'])
+    soup = BeautifulSoup(rFlagNetwork.content, "html.parser")
+
+    announcement['context'], announcement['locality'] = '', ''
+
+    context = soup.select(".node-flag-alert > div.content.clearfix > div.field.field-name-field-alert-sub-title.field-type-text.field-label-hidden > div > div")
+    if context:
+        announcement['context'] = context[0].get_text(strip=True)
+    locality = soup.select(".node-flag-alert > div.content.clearfix > div.field.field-name-field-salutation.field-type-text.field-label-hidden > div > div")
+    if locality:
+        announcement['locality'] = locality[0].get_text(strip=True)
+
+i = 0
+
 # import scraperwiki
 # import lxml.html
 #
