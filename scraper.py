39a1ffbefb60cdc0ada2ac606848491a2b73d714
Simon <email@simonvictory.com>
2018-02-21 10:14:57 +1100

    added deps and normalised the states array

--- scraper.py
+++ scraper.py
@@ -3,19 +3,34 @@
 
 from bs4 import BeautifulSoup
 import requests
+import logging
+from dateutil.parser import parse
 
 PMCTLD = "http://pmc.gov.au"
-FLAGNETWORKLINK = "http://pmc.gov.au/government/australian-national-flag/flag-network"
+FLAGNETWORKLINK = "/government/australian-national-flag/flag-network"
 
-rFlagNetwork = requests.get(FLAGNETWORKLINK)
+STATES_ABBR = ["australia-wide", "act", "nsw", "qld", "sa", "tas", "nt", "wa", "vic"]
+STATES_FULL = ["australia wide", "australian capital territory", "new south wales", "queensland", "south australia", "tasmania", "northern territory", "western australia", "victoria"]
+
+rFlagNetwork = requests.get(PMCTLD + FLAGNETWORKLINK)
 soup = BeautifulSoup(rFlagNetwork.content, "html.parser")
 
 pages = int(soup.select("#block-system-main > div > div > div > div.item-list > ul > li.pager-last.last > a")[0].attrs['href'].split("=")[1])
 
-all_announcements = []
+def is_date(string):
+    try:
+        parse(string, fuzzy=True)
+        return True
+    except ValueError as e:
+        if e.args[0] == "String does not contain a date.":
+            return False
+        return True # assume true, so it doesn't mess with the final data
+
+all_announcements = [] # pipe this into the sqlite db for morph.io
 for page in range(pages):
+    logging.info("Processing page " + str(page) + " of " + str(pages))
     if page != 0:
-        rFlagNetwork = requests.get(FLAGNETWORKLINK + "?page=" + str(page))
+        rFlagNetwork = requests.get(PMCTLD + FLAGNETWORKLINK + "?page=" + str(page))
         soup = BeautifulSoup(rFlagNetwork.content, "html.parser")
 
     page_content1 = soup.select("#block-system-main > div > div > div > div.view-content > div.views-row > div.views-field.views-field-title > h2 > a")
@@ -31,14 +46,35 @@ for announcement in all_announcements:
     rFlagNetwork = requests.get(PMCTLD + announcement['link'])
     soup = BeautifulSoup(rFlagNetwork.content, "html.parser")
 
-    announcement['context'], announcement['locality'] = '', ''
+    announcement['context'], announcement['locality'], announcement['halfMast'] = '', [], False
 
     context = soup.select(".node-flag-alert > div.content.clearfix > div.field.field-name-field-alert-sub-title.field-type-text.field-label-hidden > div > div")
     if context:
-        announcement['context'] = context[0].get_text(strip=True)
+        context_text = context[0].get_text(strip=True)
+        if not is_date(context_text):
+            announcement['context'] = context_text
+            if context_text.lower().find("half-mast") or context_text.lower().find("half mast"):
+                announcement['halfMast'] = True
     locality = soup.select(".node-flag-alert > div.content.clearfix > div.field.field-name-field-salutation.field-type-text.field-label-hidden > div > div")
     if locality:
-        announcement['locality'] = locality[0].get_text(strip=True)
+        locality_text = locality[0].get_text(strip=True)
+        if not is_date(locality_text):
+            matches = [x for x in list(zip(STATES_ABBR, STATES_FULL)) if x in locality_text.lower()]
+            # [x[0] for x in list(zip(STATES_ABBR, STATES_FULL))]
+
+            announcement['locality']
+            for i in matches:
+                for k, v in list(zip(STATES_ABBR, STATES_FULL)):
+                    i.replace(v, k)
+
+
+            #announcement['locality'] = locality_text
+
+            #[x[0] for x in states_mapping]
+            #for k, v in mapping.iteritems():
+            #    my_string = my_string.replace(k, v)
+
+
 
 i = 0
 
